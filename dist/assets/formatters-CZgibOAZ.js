48d02e06b3fda45546eed19ba1aa997d54b45706
jkrawlin <87916316+jkrawlin@users.noreply.github.com>
2025-09-25 08:49:38 +0300

    chore(hosting): add Firebase Hosting config and build; ignore dist; add .env.production for hosting builds

--- dist/assets/formatters-CZgibOAZ.js
+++
@@ -1,2 +0,0 @@
-import{s as t}from"./index-sMHxiCIB.js";const o=(r,e=t.currency)=>new Intl.NumberFormat("en-QA",{style:"currency",currency:e}).format(r);export{o as f};
-//# sourceMappingURL=formatters-CZgibOAZ.js.map
